7183cdc4e4f6e573bd340d350e40adafdb1af7be
Jerit Baiju <jeritalumkal@gmail.com>
2023-08-31 15:32:18 +0530

    avail to superuser only

--- base/views.py
+++ base/views.py
@@ -5,19 +5,20 @@ import requests
 
 
 def index(request):
-    api_key = '30cabdb500a38872a30c50a0f07c5ad8'
-    cities = ['Kumily', 'Thookkupalam']
-    weather = []
-    for city in cities:
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-        print(url)
-        response = requests.get(url, timeout=10)
-        if response.status_code == 200:
-            data = response.json()
-            weather.append({'city': data['name'], 'temp': int(data['main']['temp'])-273, 'desc': data['weather'][0]['description']})
-        else:
-            print('Error fetching weather data')
-    context = {
-        'weather': weather
-    }
-    return render(request, 'base/index.html', context)
+    if request.user.is_superuser:
+        api_key = '30cabdb500a38872a30c50a0f07c5ad8'
+        cities = ['Kumily', 'Thookkupalam']
+        weather = []
+        for city in cities:
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+            print(url)
+            response = requests.get(url, timeout=10)
+            if response.status_code == 200:
+                data = response.json()
+                weather.append({'city': data['name'], 'temp': int(data['main']['temp'])-273, 'desc': data['weather'][0]['description']})
+            else:
+                print('Error fetching weather data')
+        context = {
+            'weather': weather
+        }
+        return render(request, 'base/index.html', context)
