14ec1bf8845e771ba347fd22dd1e873f733866f6
Jerit Baiju <jeritalumkal@gmail.com>
2023-08-31 15:18:42 +0530

    initial commit

---
+++ base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+
+def index(request):
+    api_key = '30cabdb500a38872a30c50a0f07c5ad8'
+    cities = ['Kumily', 'Thookkupalam']
+    weather = []
+    for city in cities:
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+        print(url)
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            weather.append({'city': data['name'], 'temp': int(data['main']['temp'])-273, 'desc': data['weather'][0]['description']})
+        else:
+            print('Error fetching weather data')
+    context = {
+        'weather': weather
+    }
+    return render(request, 'base/index.html', context)
