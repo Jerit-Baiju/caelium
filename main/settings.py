62cbb8115358ab82149b59963b6bc6822c0a7c7e
Jerit Baiju <jeritalumkal@gmail.com>
2023-09-30 10:00:37 +0530

    django setup

--- main/settings.py
+++ main/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import django
 from dotenv import load_dotenv
 
 load_dotenv()
-
+if os.environ['ws_redis'] == 'True':
+    django.setup()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
