16e3775285e8b875b876687346e3a36a11407f2b
Jerit Baiju <jeritalumkal@gmail.com>
2023-09-30 09:55:51 +0530

    to deploy

--- main/asgi.py
+++ main/asgi.py
@@ -16,6 +16,8 @@ from django.core.asgi import get_asgi_application
 
 from chat import routing
 
+django_asgi_app = get_asgi_application()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 django.setup()
 
