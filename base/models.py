c1ee96895c680108d2e75654c2001325bcdf7bb7
Jerit Baiju <jeritalumkal@gmail.com>
2023-09-30 16:57:03 +0530

    anniversary update

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
@@ -7,7 +7,7 @@ class Relationship(models.Model):
     male = models.OneToOneField('accounts.User', on_delete=models.SET_NULL, related_name='user_1', null=True, blank=True)
     female = models.OneToOneField('accounts.User', on_delete=models.SET_NULL, related_name='user_2', null=True, blank=True)
     room = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    anniversary = models.DateField(auto_now_add=True, null=True)
+    anniversary = models.DateField(default=timezone.now, null=True, blank=True, editable=True)
 
     def __str__(self):
         return str(f'{self.male} & {self.female}')
