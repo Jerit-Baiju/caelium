dd2d54164edd3120f69caf560cefed32599867a3
Jerit Baiju <jeritalumkal@gmail.com>
2023-10-05 22:50:00 +0530

    added media urls

--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.urls import re_path as url
 from django.views.generic.base import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,11 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('favicon.ico', RedirectView.as_view(
         url=staticfiles_storage.url('favicon.png'))),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
