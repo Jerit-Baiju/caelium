e92f4c44c7316721f8c85e27b0cd76a86b2439a6
Jerit Baiju <jeritalumkal@gmail.com>
2023-10-06 07:58:38 +0530

    state event try fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,20 @@
 var protocol = 'ws';
-var user_state = 'offline'
 var domain = window.location.host
 if (debug == 'pro') {
   protocol = 'wss';
 }
 var websocketURL = protocol + '://' + domain + '/ws/socket-server/';
 const chatSocket = new WebSocket(websocketURL)
-function change_state(state){
-  if (state != user_state){
-  chatSocket.send(JSON.stringify({'type': 'status', 'content': state}))
-  }
+function change_state(state) {
+  chatSocket.send(JSON.stringify({ 'type': 'status', 'content': state }))
+  console.log('state')
 }
-change_state('online')
 window.onblur = () => {
   change_state('offline')
 }
 window.onfocus = () => {
+  change_state('online')
+}
+window.onload = () => {
   change_state('online')
 }
