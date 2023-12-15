87f0712c80b6b33b5e51e2bd42a5f998bfe04b80
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-15 18:46:35 +0530

    initialised for api

--- main/asgi.py
+++ main/asgi.py
@@ -4,26 +4,13 @@ ASGI config for main project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
 
-import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from chat import routing
-
-django_asgi_app = get_asgi_application()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-django.setup()
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(routing.websocket_urlpatterns)
-    )
-})
+application = get_asgi_application()
--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@
 URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,27 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.urls import include, path
-from django.urls import re_path as url
-from django.views.generic.base import RedirectView
-from django.views.static import serve
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    path('chat/', include('chat.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('favicon.ico', RedirectView.as_view(
-        url=staticfiles_storage.url('favicon.png'))),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
-
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
