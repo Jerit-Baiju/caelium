11a3b7b916719f2dde6796e777fdb421e425f5f3
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-22 01:59:46 +0530

    added a domain

--- main/settings.py
+++ main/settings.py
@@ -18,7 +18,9 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.43.157'
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 
