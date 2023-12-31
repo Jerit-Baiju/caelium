18dde23e25f45978b7f5c92a63332b8c895fde3b
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-31 17:11:48 +0530

    remove: simplified to just text messages

--- chats/admin.py
+++ chats/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
-from .models import Chat, ImageMessage, Message, TextMessage, VideoMessage, VoiceMessage
+from .models import Chat,Message
 
 # Register your models here.
 admin.site.register(Chat)
 admin.site.register(Message)
-admin.site.register(TextMessage)
-admin.site.register(ImageMessage)
-admin.site.register(VoiceMessage)
-admin.site.register(VideoMessage)
--- chats/models.py
+++ chats/models.py
@@ -11,29 +11,7 @@ class Chat(models.Model):
         return f"{self.pk}: {', '.join(participant_names)}"
 
 class Message(models.Model):
-    MESSAGE_TYPE_CHOICES = [
-        ('text', 'Text'),
-        ('image', 'Image'),
-        ('voice', 'Voice'),
-        ('video', 'Video'),
-    ]
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     sender = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
-    message_type = models.CharField(max_length=20, choices=MESSAGE_TYPE_CHOICES)
-
-class TextMessage(models.Model):
-    message = models.OneToOneField(Message, on_delete=models.CASCADE, primary_key=True)
     content = models.TextField()
-
-class VoiceMessage(models.Model):
-    message = models.OneToOneField(Message, on_delete=models.CASCADE, primary_key=True)
-    audio_file = models.FileField(upload_to='chats/voices/')
-
-class ImageMessage(models.Model):
-    message = models.OneToOneField(Message, on_delete=models.CASCADE, primary_key=True)
-    image = models.ImageField(upload_to='chats/images/')
-
-class VideoMessage(models.Model):
-    message = models.OneToOneField(Message, on_delete=models.CASCADE, primary_key=True)
-    video_file = models.FileField(upload_to='chats/videos/')
