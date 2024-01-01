f5b92fa81140ec9f090a4ab04300dfd8446b376b
Jerit Baiju <jeritalumkal@gmail.com>
2024-01-01 21:57:47 +0530

    test: Message View

--- chats/urls.py
+++ chats/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ChatViewSet.as_view({'get': 'list'})),
+    path('messages/<pk>/', views.MessagesView.as_view()),
     path('create/', views.ChatCreateView.as_view()),
     path('messages/create/', views.MessageCreateView.as_view()),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions, viewsets
+from rest_framework import generics, status, viewsets
+from rest_framework.response import Response
 
 from .models import Chat, Message
 from .serializers import ChatSerializer, MessageSerializer
@@ -18,7 +19,23 @@ class ChatCreateView(generics.CreateAPIView):
     serializer_class = ChatSerializer
     queryset = Chat.objects.all()
 
-class MessageViewSet(viewsets.ModelViewSet):
+class MessagesView(generics.ListAPIView):
     serializer_class = MessageSerializer
-    queryset = Message.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        chat_id = self.kwargs['pk']
+        try:
+            # Assuming 'chat_id' is the primary key of the Chat model
+            chat_messages = Message.objects.filter(chat=Chat.objects.get(id=chat_id))
+            return chat_messages
+        except Message.DoesNotExist:
+            return []
+
+    def list(self, request, *args, **kwargs):
+        chat_id = self.kwargs['pk']
+        queryset = self.get_queryset()
+        if not queryset:
+            return Response({"message": f"Chat with id {chat_id} not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
