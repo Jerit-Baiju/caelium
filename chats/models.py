44a1bb6b04618b503cdfe6963999820b20c1fcc0
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-24 19:19:26 +0530

    format: use string quotes consistently

--- chats/models.py
+++ chats/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 class Chat(models.Model):
     name = models.CharField(max_length=30, null=True, blank=True)
-    participants = models.ManyToManyField('accounts.User')
+    participants = models.ManyToManyField("accounts.User")
 
     def __str__(self):
-        participant_names = list(self.participants.values_list('username', flat=True))
+        participant_names = list(self.participants.values_list("username", flat=True))
         return f"{self.pk}: {', '.join(participant_names)}"
+
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    sender = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    sender = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
