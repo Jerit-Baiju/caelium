ead6763944cda3988ddc01c8b2c79556c86d8b8c
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-28 21:30:22 +0530

    add: token-based authentication to ChatConsumer and update routing

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,27 +1,75 @@
 import json
 
+import jwt
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.conf import settings
+
+from accounts.models import User
+from chats.serializers import MessageCreateSerializer
 
 
 class ChatConsumer(WebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.chat_id = None
+        self.group_name = None
+        self.user = None
+
     def connect(self):
         self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
-        self.group_name = f"chat_{self.chat_id}"
-
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
-        self.accept()
-        self.send(text_data=json.dumps({"message": f"joined {self.chat_id}"}))
+        self.group_name = self.chat_id
+        token = self.scope["url_route"]["kwargs"]["token"]
+        try:
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            user_id = decoded_token["user_id"]
+            self.user = User.objects.get(id=user_id)
+            async_to_sync(self.channel_layer.group_add)(
+                self.group_name, self.channel_name
+            )
+            self.accept()
+        except jwt.ExpiredSignatureError:
+            self.close()
+        except jwt.DecodeError:
+            self.close()
+        except User.DoesNotExist:
+            self.close()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name, {"type": "chat_message", "message": message}
-        )
+        content = text_data_json.get("content")
+        token = text_data_json.get("token")
+        try:
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            user_id = decoded_token["user_id"]
+            user = User.objects.get(id=user_id)
+        except Exception as e:
+            print("Token authentication failed:", e)
+            return
+        data = {
+            "chat": self.chat_id,
+            "sender": user.id,
+            "content": content,
+        }
+        create_serializer = MessageCreateSerializer(data=data)
+        if create_serializer.is_valid():
+            saved_message = create_serializer.save()
+            message = {
+                "message_id": saved_message.id,
+                "user_id": saved_message.sender.id,
+                "content": saved_message.content,
+                "name": saved_message.sender.name,
+                "timestamp": saved_message.timestamp.strftime("%Y-%m-%dT%H:%M:%S"),
+                "avatar": saved_message.sender.avatar.url,
+            }
+            print(message)
+            async_to_sync(self.channel_layer.group_send)(
+                self.group_name, {"type": "chat_message", "message": message}
+            )
+        else:
+            print(create_serializer.errors)
 
     def chat_message(self, event):
         message = event["message"]
-
-        self.send(text_data=json.dumps({"type": "message", "message": message}))
+        self.send(text_data=json.dumps({"type": "chat", "message": message}))
