e28a464a6ee4808c54cd7faf1d0f8394dd7de151
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-04 17:53:01 +0530

    add: list method to CraftViewSet to limit content length

--- crafts/views.py
+++ crafts/views.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from crafts.models import Craft
@@ -9,3 +10,10 @@ class CraftViewSet(ModelViewSet):
     queryset = Craft.objects.all()
     serializer_class = CraftSerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        for craft in queryset:
+            craft.content = " ".join(craft.content.split()[:60])
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
