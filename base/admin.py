35c0d307d367a51702ca23fdc790285f9da5299e
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-04 10:39:22 +0530

    add: base models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from base.models import Couple, Family, Work
+
 # Register your models here.
+
+admin.site.register(Couple)
+admin.site.register(Family)
+admin.site.register(Work)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+SPACES = (
+    ("personal", "Personal"),
+    ("partner", "Partner"),
+    ("family", "Family"),
+    ("all", "All"),
+)
+
+
+class Couple(models.Model):
+    members = models.ManyToManyField(User)
+    anniversary = models.DateTimeField()
+
+
+class Family(models.Model):
+    members = models.ManyToManyField(User)
+
+    class Meta:
+        verbose_name_plural = "Families"
+
+
+class Work(models.Model):
+    members = models.ManyToManyField(User)
+
+
+class JoinSpaceRequest(models.Model):
+    space = models.CharField(max_length=25, choices=SPACES)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    confirmed = models.BooleanField(default=False)
