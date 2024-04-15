3f5e0e7f78d81f9edf002ea060b01d536477df96
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-15 09:23:13 +0530

    format: add required lines between

--- accounts/views.py
+++ accounts/views.py
@@ -4,22 +4,25 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
-from .serializers import (MyTokenObtainPairSerializer, RegisterSerializer,
-                          UserSerializer)
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
+
     def get_queryset(self):
         return User.objects.all()
+
 
 class UserDetailsView(RetrieveAPIView):
     queryset = User.objects.all()
