3cd21472e8c3227d7235a7391f65c71a29832d64
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-17 00:47:27 +0530

    fix: redirect issues

--- main/settings.py
+++ main/settings.py
@@ -190,5 +190,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
-SECURE_SSL_REDIRECT = True
