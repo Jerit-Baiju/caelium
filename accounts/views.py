cacbd495d0355b77b179ed0976b851de9bb82d4e
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-28 14:53:27 +0530

    add: response and permission checks in UserUpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, status, viewsets
 from rest_framework.generics import RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
@@ -37,3 +38,12 @@ class UserDetailsView(RetrieveAPIView):
 class UserUpdateView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
+
+    def perform_update(self, serializer):
+        user_instance = serializer.instance
+        if user_instance == self.request.user:
+            serializer.save()
+        return Response(
+            {"error": "You are not authorized to update this user's data."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
