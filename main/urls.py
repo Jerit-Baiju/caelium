161ef9b2b51bcc2f13129bbdde4b1f2b666fdcfe
Jerit Baiju <jeritalumkal@gmail.com>
2024-05-24 13:44:20 +0530

    format: changed quotes

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/auth/', include('accounts.urls')),
-    path('api/chats/', include('chats.urls')),
-    path('api/crafts/', include('crafts.urls')),
-    path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
+    path("api/auth/", include("accounts.urls")),
+    path("api/chats/", include("chats.urls")),
+    path("api/crafts/", include("crafts.urls")),
+    path("admin/", admin.site.urls),
+    path("api/", include("base.urls")),
 ]
 
 if settings.DEBUG:
