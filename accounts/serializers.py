a48e33eeb446bd5762f1d7ddbbdd8e1a87261f0b
Jerit Baiju <jeritalumkal@gmail.com>
2024-06-03 23:25:49 +0530

    add: avatar field to UserSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name']
+        fields = ['id', 'email', 'name', 'avatar']
