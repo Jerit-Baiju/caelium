6683167363e13f20d54efd4a1eacd60515c78851
Jerit Baiju <jeritalumkal@gmail.com>
2024-06-15 22:08:29 +0530

    update: permission check in CraftViewSet's destroy method

--- crafts/views.py
+++ crafts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 
 from crafts.models import Craft
 from crafts.serializers import CraftSerializer
@@ -17,3 +18,12 @@ class CraftViewSet(ModelViewSet):
         for craft in serializer.data:
             craft["content"] = " ".join(craft["content"].split()[:60])
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        craft = self.get_object()
+        if craft.owner != request.user:
+            return Response(
+                {"detail": "You do not have permission to delete this craft."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        return super().destroy(request, *args, **kwargs)
