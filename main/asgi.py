1a4d3a268468aafc050155e69b7afad0a5b6a0ed
Jerit Baiju <jeritalumkal@gmail.com>
2024-09-07 18:04:58 +0000

    Fix: Resolve circular import issue in ASGI application
    The ASGI application initialization has been updated to resolve a circular import issue. The import of the `chats.routing` module, which depends on Django settings, has been moved after the `django.setup()` call. This ensures that Django settings are properly loaded before importing modules that rely on them.

--- main/asgi.py
+++ main/asgi.py
@@ -1,16 +1,18 @@
 import os
+
 import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-django.setup()
-from chats import routing
-
 # Ensure the DJANGO_SETTINGS_MODULE environment variable is set
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 # Initialize Django ASGI application early to ensure the settings are loaded
+django.setup()
+
+# Import routing after Django setup
+from chats import routing
 
 # Get the ASGI application
 django_asgi_app = get_asgi_application()
@@ -19,8 +21,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(routing.websocket_urlpatterns)
-        ),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
