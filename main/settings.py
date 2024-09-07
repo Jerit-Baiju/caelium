24cbc248e3fc469fa560a08dd49333a5ccbc78f5
Jerit Baiju <jeritalumkal@gmail.com>
2024-09-07 19:12:47 +0000

    Fix: Middleware Ordering for Static File Serving
    Reordered middleware to ensure WhiteNoiseMiddleware precedes SecurityMiddleware. This allows WhiteNoise to properly serve static files, preventing potential conflicts and improving security.

--- main/settings.py
+++ main/settings.py
@@ -54,13 +54,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"
