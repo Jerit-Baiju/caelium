810a937805dbb3ab347940ddb3e95bfd27cf0ba4
Jerit Baiju <jeritalumkal@gmail.com>
2024-09-08 06:33:47 +0000

    Remove Couple model from admin interface
    The Couple model is no longer accessible through the admin interface for improved data management and consistency.

--- base/admin.py
+++ base/admin.py
@@ -4,7 +4,7 @@ from base.models import Couple, Event, Family, Task, Work
 
 # Register your models here.
 
-admin.site.register(Couple)
+# admin.site.register(Couple)
 admin.site.register(Family)
 admin.site.register(Work)
 admin.site.register(Task)
