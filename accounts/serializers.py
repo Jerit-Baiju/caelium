94bb177705dc02668405c8e895a9f6c16d39108a
Jerit Baiju <jeritalumkal@gmail.com>
2025-01-14 12:45:23 +0530

    refactor(users): add username field to UserSerializer for improved user identification

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+
 from .models import FCMToken, User
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'avatar', 'location', 'gender', 'birthdate', 'last_seen']
+        fields = ["id", "email", "name", "avatar", "location", "gender", "birthdate", "last_seen", "username"]
 
 
 class FCMTokenSerializer(ModelSerializer):
