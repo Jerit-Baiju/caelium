62d6499b1232111f2205daca61e0940615de0615
Jerit Baiju <jeritalumkal@gmail.com>
2025-02-01 14:57:11 +0530

    refactor(chats): remove unused mixins and serializers from ChatViewSet for improved code clarity

--- chats/views.py
+++ chats/views.py
@@ -1,16 +1,14 @@
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 
 from accounts.models import User
 from accounts.serializers import UserSerializer
 
 from .models import Chat, Message, PinnedChat
-from .serializers import ChatSerializer, MessageCreateSerializer, MessageSerializer, PinnedChatSerializer
+from .serializers import ChatSerializer, MessageCreateSerializer, MessageSerializer
 
 
 class MessagePagination(PageNumberPagination):
