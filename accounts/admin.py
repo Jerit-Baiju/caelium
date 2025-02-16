ef506e1d4ea891fc642348f78ecdfe9888dbd561
Jerit Baiju <jeritalumkal@gmail.com>
2025-02-16 12:11:27 +0530

    feat(accounts): add TestUserEmail model and update Google login to allow approved test emails

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import FCMToken, GoogleToken, User
+from .models import FCMToken, GoogleToken, TestUserEmail, User
 
 admin.site.register(User)
 admin.site.register(GoogleToken)
 admin.site.register(FCMToken)
+admin.site.register(TestUserEmail)
