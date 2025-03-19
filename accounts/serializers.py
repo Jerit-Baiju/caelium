94b3200716df185f7821804f1bc757765a32d9b0
Jerit Baiju <jeritalumkal@gmail.com>
2025-03-19 15:28:29 +0530

    feat(accounts): add is_special_user field to UserSerializer to indicate SpecialUser status

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from .models import FCMToken, User
+from accounts.models import FCMToken, SpecialUser, User
 
 
 class UserSerializer(ModelSerializer):
+    is_special_user = SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["id", "email", "name", "avatar", "location", "gender", "birthdate", "last_seen", "username"]
+        fields = [
+            "id",
+            "email",
+            "name",
+            "avatar",
+            "location",
+            "gender",
+            "birthdate",
+            "last_seen",
+            "username",
+            "is_special_user",
+        ]
+
+    def get_is_special_user(self, obj):
+        """Check if user's email is in the SpecialUser table"""
+        return SpecialUser.is_special_user(obj.email)
 
 
 class FCMTokenSerializer(ModelSerializer):
