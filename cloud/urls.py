8240bc787548b687c140284246b48761b7ff4e8e
Jerit Baiju <jeritalumkal@gmail.com>
2025-03-24 18:29:36 +0530

    feat(cloud): add GalleryListView for media file listing; remove FileListView

--- cloud/urls.py
+++ cloud/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from cloud.views import DirectoryDetailView, DirectoryListView, FileDownloadView, FileListView, FileUploadView
+from cloud.views import (DirectoryDetailView, DirectoryListView, FileDownloadView, FileUploadView, GalleryListView)
 
 router = DefaultRouter()
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("upload/", FileUploadView.as_view(), name="file-upload"),
     path("files/<uuid:pk>/download/", FileDownloadView.as_view(), name="file-download"),
-    path("files/", FileListView.as_view(), name="file-list"),
+    path("gallery/", GalleryListView.as_view(), name="gallery-list"),
     path("directories/", DirectoryListView.as_view(), name="directory-list"),
     path("directories/<uuid:pk>/", DirectoryDetailView.as_view(), name="directory-detail"),
 ]
