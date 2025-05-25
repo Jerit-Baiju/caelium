6765ce2b808b1bbefbd845c8be066aeb28294b63
Jerit Baiju <jeritalumkal@gmail.com>
2025-05-25 12:11:25 +0530

    feat(admin): register Account model in the admin interface

--- finance/admin.py
+++ finance/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from finance.models import Label, Transaction
+from finance.models import Account, Label, Transaction
 
 # Register your models here.
 
 admin.site.register(Label)
 admin.site.register(Transaction)
+admin.site.register(Account)
